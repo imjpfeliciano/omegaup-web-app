a99e00972c351a2b24389c3fbe8975702d46d615
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2025-01-22 13:31:07 -0600

    feat: init next project

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+// TODO:
+// - If user is not logged in, then display landing page
+// - If user is logged in, then display problemset
+export default function Home() {
+  return (
+    <div>
+      <h1>Page</h1>
+    </div>
+  );
+}
