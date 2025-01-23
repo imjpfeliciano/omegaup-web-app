39065574b7407b4c9ef198ae5b014bd6204aaa11
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2025-01-23 10:25:38 -0600

    fix: initial login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -3,36 +3,41 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 
 const { OMEGAUP_API_TOKEN: token, NEXTAUTH_URL: LoginEndpoint, NEXTAUTH_SECRET } = process.env;
 
+
+
 const options: NextAuthOptions = {
-    // secret: NEXTAUTH_SECRET,
+    secret: NEXTAUTH_SECRET,
     providers: [
         CredentialsProvider({
+            
             name: 'Credentials',
             credentials: {
-            username: { label: 'Username', type: 'text', placeholder: 'jsmith' },
+            usernameOrEmail: { label: 'Username', type: 'text', placeholder: 'jsmith' },
             password: { label: 'Password', type: 'password' },
             },
             async authorize(credentials) {
-                return {
-                    id: '1',
-                    name: 'J Smith',
-                    email: credentials?.username,
+                const searchParams = new URLSearchParams(credentials);
+                // For the moment we are not using the csrfToken
+                searchParams.delete('csrfToken');
+                console.log({ searchParams });
+                
+                const omegaupLoginUrl = `${LoginEndpoint}/${searchParams.toString()}`;
+
+                console.log({ omegaupLoginUrl });
+
+                const res = await fetch(omegaupLoginUrl, {
+                    headers: {
+                        Authorization: `token ${token}`,
+                    }
+                })
+
+                const user = await res.json();
+
+                if (user) {
+                    return user;
                 }
-                // const searchParams = new URLSearchParams(credentials);
-                // console.log({ searchParams });
-                // const res = await fetch(`${LoginEndpoint}/${searchParams}`, {
-                //     headers: {
-                //         Authorization: `Token ${token}`,
-                //     }
-                // })
 
-                // const user = await res.json();
-
-                // if (user) {
-                //     return user;
-                // }
-
-                // return null;
+                return null;
             }
         }),
     ]
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,21 @@
 // - If user is logged in, then display problemset
 'use client';
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
-  const { data: session } = useSession();
+  const router = useRouter();
+  const { data: session, status } = useSession();
+
+  if (status === 'loading') {
+    return <p>Loading...</p>
+  }
 
   console.log({ session })
+
+  if (!session) {
+    router.push('/api/auth/signin');
+  }
 
   return (
     <div>
