59503e443db82bdd5b76e53e3c3f6b2c4a6b2844
Jonathan Peraza Feliciano <jperazafeliciano@gmail.com>
2025-02-06 16:49:23 -0600

    feat: add content type to request

--- src/app/api/problems/route.ts
+++ src/app/api/problems/route.ts
@@ -1,13 +1,14 @@
 const { OMEGAUP_API_URL, OMEGAUP_API_TOKEN } = process.env;
 
 export async function GET() {
-    const res = await fetch(`${OMEGAUP_API_URL}/problem/list`, {
-        headers: {
-            Authorization: `token ${OMEGAUP_API_TOKEN}`
-        }
-    })
+  const res = await fetch(`${OMEGAUP_API_URL}/problem/list`, {
+    headers: {
+      Authorization: `token ${OMEGAUP_API_TOKEN}`,
+      "Content-Type": "application/json",
+    },
+  });
 
-    const data = await res.json();
+  const data = await res.json();
 
-    return Response.json({ data})
+  return Response.json({ data });
 }
