9e9d4c7e3ad72f0e619436b9a0205ac21cff656c
Jonathan Peraza <jonathan.peraza@cerby.com>
2025-02-08 20:23:25 -0600

    feat: update login form

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -1,6 +1,6 @@
 "use client";
-import clsx from "clsx";
-import { getCsrfToken, getProviders, signIn } from "next-auth/react";
+import Card from "@/components/UI/Card";
+import { getCsrfToken, getProviders } from "next-auth/react";
 import { useEffect, useState } from "react";
 
 interface ProviderBase {
@@ -25,46 +25,49 @@ export default function Page() {
   }, []);
 
   return (
-    <div className="flex flex-col lg:gap-4">
-      {/* Email / password login form */}
-      <form
-        className="flex flex-col bg-amber-50 p-16 rounded-lg gap-4 self-center"
-        method="post"
-        action="/api/auth/callback/credentials"
-      >
-        <input type="hidden" name="csrfToken" value={csrfToken} />
-        <div className="flex flex-col lg:max-w-[75%]">
-          <div className="flex flex-col">
-            <label htmlFor="email">
-              Email
-              <input
-                className="border shadow-md"
-                name="usernameOrEmail"
-                id="usernameOrEmail"
-              />
-            </label>
-          </div>
-          <div className="flex flex-col">
-            <label htmlFor="password">
-              Password
-              <input
-                className="border shadow-md"
-                name="password"
-                id="password"
-                type="password"
-              />
-            </label>
-          </div>
-        </div>
-        <input
-          type="submit"
-          value="Login"
-          className="bg-red-500 cursor-pointer px-3 py-1 rounded-lg font-semibold text-white"
-        />
-      </form>
+    <div className="max-w-lg mx-auto">
 
-      {/*  (Google, etc.) form */}
-      {providers &&
+
+      <Card>
+        {/* Email / password login form */}
+        <form
+          className="flex flex-col gap-4 p-16 rounded-lg gap-4 self-center"
+          method="post"
+          action="/api/auth/callback/credentials"
+        >
+          <input type="hidden" name="csrfToken" value={csrfToken} />
+          <div className="flex flex-col gap-4">
+            <div className="flex flex-col gap-2 w-full">
+              <label htmlFor="email">
+                <span className="font-semibold text-slate-700">Email or user account</span>
+                <input
+                  className="border shadow-md w-full p-2 text-slate-500 focus:outline-none"
+                  name="usernameOrEmail"
+                  id="usernameOrEmail"
+                />
+              </label>
+            </div>
+            <div className="flex flex-col gap-2">
+              <label htmlFor="password">
+                <span className="font-semibold text-slate-700">Password</span>
+                <input
+                  className="border shadow-md w-full p-2 text-slate-500 focus:outline-none"
+                  name="password"
+                  id="password"
+                  type="password"
+                />
+              </label>
+            </div>
+          </div>
+          <input
+            type="submit"
+            value="Login with OmegaUp Credentials"
+            className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center justify-between dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          />
+        </form>
+
+        {/*  (Google, etc.) form - Not supported yet*/}
+        {/* {providers &&
         Object.values(providers).map((provider) => (
           <div key={provider.name} className="self-center">
             <button
@@ -76,7 +79,8 @@ export default function Page() {
               <span>Connectez-vous avec {provider.name}</span>
             </button>
           </div>
-        ))}
+        ))} */}
+      </Card>
     </div>
   );
 }
